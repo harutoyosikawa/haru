93cbca80e1cdc0368ab2ff01ddaaa30be10aa3f5
harutoyosikawa <charizardxs@icloud.com>
2025-11-20 16:25:15 +0900

    Update app 1.py

--- app 1.py
+++ app 1.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
 st.title("こんにちは、吉村ゼミ")
+
 name = st.text_input("好きな人はなんですか")
 
 st.write(name) 
 
-camera_photo = st.camera_imput("ハイチーズ!") 
+camera = st.camera_imput("ハイチーズ!") 
 if camera:
   st.image(camera, caption="写真",use_column_width=True)
   
